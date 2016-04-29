c10d3f0838f2e0e661766d94cd5dd533450b89e7
AnthonyCorrado <amcstudios@yahoo.com>
2016-04-29 15:15:26 -0700

    adds angular 2 base setup with ng2-material

--- src/client/app/navamid.component.ts
+++ src/client/app/navamid.component.ts
@@ -1,21 +1,19 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
+import {TopNavComponent} from './top-nav/top-nav.component'
+
 @Component({
   moduleId: __moduleName,
   selector: 'navamid-app',
   providers: [ROUTER_PROVIDERS],
   templateUrl: 'navamid.component.html',
+  styles: ['.body { margin: 0;, padding: 0 }'],
   styleUrls: ['navamid.component.css'],
-  directives: [ROUTER_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES, TopNavComponent],
   pipes: []
 })
 @RouteConfig([
 ])
 export class NavamidApp {
-  defaultMeaning: number = 42;
-
-  meaningOfLife(meaning?: number) {
-    return `The meaning of life is ${meaning || this.defaultMeaning}`;
-  }
 }
---
+++ src/client/app/top-nav/top-nav.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from 'angular2/core';
+
+// import {MATERIAL_DIRECTIVES, MATERIAL_PROVIDERS} from 'ng2-material/all';
+
+
+@Component({
+  moduleId: __moduleName,
+  selector: 'top-nav',
+  templateUrl: 'top-nav.component.html',
+  styleUrls: ['top-nav.component.css'],
+  directives: []
+})
+export class TopNavComponent implements OnInit {
+
+  constructor() {}
+  
+  ngOnInit() {
+  }
+
+}
